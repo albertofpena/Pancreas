75dadf9f0f4938abda45b5be06a73bce38aec8d8
Alberto Fernández Pena <afpena@gbt.tfo.upm.es>
2015-07-07 21:37:26 +0200

    removed build folder

--- build/CMakeFiles/volumereconstructor_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "volumereconstructor_automoc.cpp"
-  "CMakeFiles/volumereconstructor_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/volumereconstructor_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
